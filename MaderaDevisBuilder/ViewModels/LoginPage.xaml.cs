a9cbdf73b79642fc8671516c6d7d01d9108675ab
Alolu <borniche.leo@gmail.com>
2020-01-04 16:47:49 +0100

    Ajout logo + logique login

--- MaderaDevisBuilder/ViewModels/LoginPage.xaml.cs
+++ MaderaDevisBuilder/ViewModels/LoginPage.xaml.cs
@@ -8,26 +8,48 @@ namespace MaderaDevisBuilder.ViewModels
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
-        public LoginViewModel viewModel { get; set; } = new LoginViewModel();
 
         public LoginPage()
         {
             NavigationPage.SetHasNavigationBar(this, false);
-            InitializeComponent();
-            BindingContext = viewModel;
             NavigationPage.SetHasBackButton(this, false);
+            InitializeComponent();
         }
 
+        //Empeche de retourner a la derniere page en appuyant sur le bouton retour
         protected override bool OnBackButtonPressed()
         {
             return true;
         }
 
-        async void OnNextPageButtonClicked(object sender, EventArgs e)
+        //Baisse l'opacité du bouton progressivement (Animation)
+        void RemoveOpacity(object sender, EventArgs e)
         {
-            if (viewModel.CheckCredentials())
+            Entry input = (Entry)sender;
+            input.FadeTo(0.5, 1000);
+        }
+
+        //Augmente l'opacité du bouton progressivement (Animation)
+        void AddOpacity(object sender, EventArgs e)
+        {
+            Entry input = (Entry)sender;
+            input.FadeTo(1, 1000);
+        }
+
+        //Echange l'input de mail avec celui de password, et ensuite affiche le menu. (Animation)
+        void ShowNext(object sender, EventArgs e)
+        {
+            if (EmailEntry.IsVisible)
             {
-                await Navigation.PushAsync(new MenuPage());
+                EmailEntry.FadeTo(0, 300, Easing.SinIn);
+                EmailEntry.IsVisible = false;
+                PassEntry.IsVisible = true;
+                PassEntry.FadeTo(1, 300, Easing.SinOut);
+                PassEntry.Focus();
+            }
+            else
+            {
+                Navigation.PushAsync(new MenuPage());
             }
         }
     }
