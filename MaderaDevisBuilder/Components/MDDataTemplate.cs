5fe6e5b9608649f2e2e79f43d3dafc3c6ff0eeb0
Alolu <borniche.leo@gmail.com>
2020-01-05 09:05:26 +0100

    Page menu bientot fini

---
+++ MaderaDevisBuilder/Components/MDDataTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MaderaDevisBuilder.Components
+{
+    public class MDDataTemplate : DataTemplateSelector
+    {
+        public DataTemplate EvenTemplate { get; set; }
+        public DataTemplate UnevenTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((List<string>)((ListView)container).ItemsSource).IndexOf(item as string) % 2 == 0 ? EvenTemplate : UnevenTemplate;
+        }
+    }
+}
--- MaderaDevisBuilder/ViewModels/MenuPage.xaml.cs
+++ MaderaDevisBuilder/ViewModels/MenuPage.xaml.cs
@@ -7,11 +7,26 @@ namespace MaderaDevisBuilder.ViewModels
 {
     public partial class MenuPage : ContentPage
     {
+        public List<string> Items { get; set; } = new List<string>()
+        {
+            "Léo Borniche",
+            "Jérome Dupire",
+            "Vanessa Merle",
+            "Romain Bouchez",
+            "Alexis Planchon",
+            "Jimmy Turpin",
+            "Charles Theteen",
+            "Michel Saouma",
+            "Vincent Fouger",
+            "Ambroise De Lima"
+        };
+
         public MenuPage()
         {
             NavigationPage.SetHasNavigationBar(this, false);
             NavigationPage.SetHasBackButton(this, false);
             InitializeComponent();
+            BindingContext = this;
         }
         protected override bool OnBackButtonPressed()
         {
